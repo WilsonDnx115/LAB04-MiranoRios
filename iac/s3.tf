ac3176705a106fbe8b66d30976d40861bbf91720
WilsonDnx115 <wilsondanix@gmail.com>
2026-05-06 18:14:59 -0500

    feat: linea para forzar la destruccion del s3

--- iac/s3.tf
+++ iac/s3.tf
@@ -89,14 +89,11 @@ resource "aws_s3_bucket_notification" "bucket_notification" {
   depends_on = [aws_sqs_queue_policy.image_queue_policy]
 }
 
-# Subir imagen inicial de prueba
+# Subir imagen inicial
 resource "aws_s3_object" "initial_image" {
   bucket       = aws_s3_bucket.images.id
   key          = "uploads/image.jpeg"
   source       = "${path.module}/../src/images/image.jpeg"
   content_type = "image/jpeg"
-
-  # Asegurarnos de que se suba DESPUÉS de configurar las notificaciones
-  # para que dispare el pipeline correctamente.
   depends_on   = [aws_s3_bucket_notification.bucket_notification]
 }
