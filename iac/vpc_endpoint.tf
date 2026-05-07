e55245a18f4ca4c0b778a35c388be339173b22d0
WilsonDnx115 <wilsondanix@gmail.com>
2026-05-06 21:33:40 -0500

    feat: ajustar endpoints de vpc para acceso privado a s3 y sqs

--- iac/vpc_endpoint.tf
+++ iac/vpc_endpoint.tf
@@ -6,7 +6,8 @@ resource "aws_vpc_endpoint" "s3" {
   vpc_endpoint_type = "Gateway"
 
   route_table_ids = [
-    aws_route_table.private.id
+    aws_route_table.private_a.id,
+    aws_route_table.private_b.id
   ]
 
   policy = jsonencode({
@@ -24,30 +25,42 @@ resource "aws_vpc_endpoint" "s3" {
 
 #PRIVADO
 
-# Tabla de rutas privada
-resource "aws_route_table" "private" {
+# Tablas de rutas privadas (A y B)
+resource "aws_route_table" "private_a" {
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_a.id
+  }
+
   tags = {
-    Name = "${var.project_name}-${terraform.workspace}-private-rt"
+    Name = "${var.project_name}-${terraform.workspace}-private-rt-a"
+  }
+}
+
+resource "aws_route_table" "private_b" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat_b.id
+  }
+
+  tags = {
+    Name = "${var.project_name}-${terraform.workspace}-private-rt-b"
   }
 }
 
 # Asociaciones privadas
 resource "aws_route_table_association" "private_a" {
   subnet_id      = aws_subnet.private_a.id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private_a.id
 }
 
 resource "aws_route_table_association" "private_b" {
   subnet_id      = aws_subnet.private_b.id
-  route_table_id = aws_route_table.private.id
-}
-
-# Conectar s3 a la tabla
-resource "aws_vpc_endpoint_route_table_association" "s3_main" {
-  route_table_id  = aws_route_table.private.id
-  vpc_endpoint_id = aws_vpc_endpoint.s3.id
+  route_table_id = aws_route_table.private_b.id
 }
 
 # Endpoint para sqs
@@ -88,4 +101,4 @@ resource "aws_route_table_association" "public_a" {
 resource "aws_route_table_association" "public_b" {
   subnet_id      = aws_subnet.public_b.id
   route_table_id = aws_route_table.public.id
-}
+} 
