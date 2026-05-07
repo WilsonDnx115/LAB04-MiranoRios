1b626204aa1fdcce423fe0ece205b5c4bed52343
WilsonDnx115 <wilsondanix@gmail.com>
2026-05-06 21:31:22 -0500

    refactor: correcion en el nombre del recruso nat_b

--- iac/nat.tf
+++ iac/nat.tf
@@ -17,3 +17,21 @@ resource "aws_eip" "nat_b" {
   }
 }
 
+# NAT Gateways
+resource "aws_nat_gateway" "nat_a" {
+  allocation_id = aws_eip.nat_a.id
+  subnet_id     = aws_subnet.public_a.id
+
+  tags = {
+    Name = "${var.project_name}-${terraform.workspace}-nat-a"
+  }
+}
+
+resource "aws_nat_gateway" "nat_b" {
+  allocation_id = aws_eip.nat_b.id
+  subnet_id     = aws_subnet.public_b.id
+
+  tags = {
+    Name = "${var.project_name}-${terraform.workspace}-nat-b"
+  }
+}
